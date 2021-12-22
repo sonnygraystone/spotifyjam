32f71bd34149b081e8699c09879acb872d0d5b1b
sonnygraystone <sonny.nguyen26@gmail.com>
2021-12-22 22:27:36 +1300

    Update className
    Update className

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -16,12 +16,12 @@ handleNameChange(event) {
 
     render() {
         return ( 
-                <div class="Playlist">
+                <div className="Playlist">
                         <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
                         <TrackList tracks={this.props.playlistTracks}
                         onRemove={this.props.onRemove}
                         isRemoval={true}/>
-                        <button class="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+                        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
                 </div>
         )
     }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,7 +7,7 @@ import Track from '../Track/Track';
 class TrackList extends React.Component {
     render() {
         return (
-            <div class="TrackList">
+            <div className="TrackList">
                 {
                     this.props.tracks.map(track => {
                         return <Track track={track}

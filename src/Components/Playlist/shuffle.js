2af268611ef3b7be2ba98eec5c3f165697df36d5
sonnygraystone <89627466+sonnygraystone@users.noreply.github.com>
2026-06-21 14:42:05 +1200

    feat: add shuffle util

---
+++ src/Components/Playlist/shuffle.js
@@ -0,0 +1,9 @@
+// pr-demo: Fisher-Yates shuffle for the playlist
+export function shuffle(tracks) {
+  const a = [...tracks];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
